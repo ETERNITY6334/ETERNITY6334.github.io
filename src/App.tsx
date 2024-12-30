3a27b23d868c7e730daeab8b07d2c56c38171977
ETERNITY6334 <hseunghwan74@gmail.com>
2024-12-30 18:06:46 +0900

    re

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import useImageGeneration from './hooks/useImageGeneration';
 
 function App() {
-  const {
-    url,
-    submit,
-    modelModal,
-    setModelModal,
-    model,
-    setModel,
-    timer,
-    second,
-    setSecond,
-    handleSubmit,
-    loginModal,
-    setloginModal,
-    loggedIn,
-    setLoggedIn
-  } = useImageGeneration();
-
   return (
-    <div className='body'>
-      <Sidebar />
-      <Header
-        loginModal={loginModal}
-        setloginModal={setloginModal}
-        loggedIn={loggedIn}
-        setLoggedIn={setLoggedIn}
-      />
-      <Main
-        url={url}
-        submit={submit}
-        modelModal={modelModal}
-        setModelModal={setModelModal}
-        model={model}
-        setModel={setModel}
-        timer={timer}
-        second={second}
-        setSecond={setSecond}
-        handleSubmit={handleSubmit}
-      />
-      <Footer />
+    <div className="App">
+      <h1>리액트깃허브페이지</h1>
     </div>
   );
 }
