364c3ee210b4d263125094614e1b139a157ab3db
ETERNITY6334 <hseunghwan74@gmail.com>
2024-12-30 20:45:58 +0900

    c

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
-ReactDOM.render((
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
-))  
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
